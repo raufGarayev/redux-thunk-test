5c24914f590785fc773f0f8dfa445e00c877758d
GARAYEV-PG <rafkagarayev@gmail.com>
2023-04-16 15:13:04 +0400

    working with redux-thunk

---
+++ src/containers/ProductDetail.js
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProduct, removeSelectedProduct } from '../redux/actions/productActions'
+
+const ProductDetail = () => {
+    const product = useSelector(state => state.product)
+    const {image, title, price, category, description} = product
+    const {productId} = useParams()
+    const dispatch = useDispatch()
+
+    
+
+    useEffect(() => {
+        if(productId && productId !== "") {
+            dispatch(fetchProduct(productId))
+        }
+        return () => {
+            dispatch(removeSelectedProduct())
+        }
+    }, [productId])
+
+  return (
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider"></div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} alt={title} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <span className="ui teal tag label">${price}</span>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProductDetail
---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,18 @@
+import { ActionTypes } from "../constants/action-types";
+import fakeStoreApi from "../../apis/fakeStoreApi";
+
+export const fetchProducts = () => async (dispatch) =>  {
+    const res = await fakeStoreApi.get("/products")
+    dispatch({type: ActionTypes.FETCH_PRODUCTS, payload: res.data})
+}
+
+export const fetchProduct = (id) => async (dispatch) =>  {
+    const res = await fakeStoreApi.get(`/products/${id}`)
+    dispatch({type: ActionTypes.FETCH_PRODUCT, payload: res.data})
+}
+
+export const removeSelectedProduct = () => {
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+  };
+};
